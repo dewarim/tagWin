19634260e02f82c9ae7e1aeb0a98fd945514ef8c
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-02-12 11:17:36 +0100

    Updated issue management and scm entries.

--- TagWinGrailsPlugin.groovy
+++ TagWinGrailsPlugin.groovy
@@ -33,10 +33,10 @@ Open source tag library for Grails applications
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "github", url: "https://github.com/dewarim/tagWin/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/dewarim/tagWin" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
